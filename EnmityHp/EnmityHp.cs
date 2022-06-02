98c6930cd897cce170a2d5ec44a790908c667038
Eternita-S <eternitast@gmail.com>
2022-06-02 10:00:09 +0300

    upd

--- EnmityHp/EnmityHp.cs
+++ EnmityHp/EnmityHp.cs
@@ -20,69 +20,26 @@ namespace EnmityHp
     unsafe class EnmityHp : IDalamudPlugin
     {
         public string Name => "EnmityHp";
-        internal List<(float x, float y, string str)> drawList;
         private Config Cfg;
         bool CfgOpen = false;
 
         public void Dispose()
         {
-            Svc.Framework.Update -= Tick;
             Svc.PluginInterface.UiBuilder.Draw -= Draw;
         }
 
         public EnmityHp(DalamudPluginInterface pluginInterface)
         {
             pluginInterface.Create<Svc>();
-            drawList = new List<(float x, float y, string str)>();
-            Svc.Framework.Update += Tick;
             Svc.PluginInterface.UiBuilder.Draw += Draw;
             Cfg = Svc.PluginInterface.GetPluginConfig() as Config ?? new Config();
             Svc.PluginInterface.UiBuilder.OpenConfigUi += delegate { CfgOpen = true; };
         }
-
-        private void Tick(Framework framework)
-        {
-            drawList.Clear();
-            if (!Svc.Condition[ConditionFlag.InCombat] || Svc.ClientState.IsPvP) return;
-
-            try
-            {
-                var enlist = Svc.GameGui.GetAddonByName("_EnemyList", 1);
-                if (enlist != IntPtr.Zero)
-                {
-                    var enlistAtk = (AtkUnitBase*)enlist;
-                    if (enlistAtk->UldManager.NodeListCount < 12) return;
-                    var baseX = enlistAtk->X;
-                    var baseY = enlistAtk->Y;
-                    if (enlistAtk->IsVisible)
-                    {
-                        for (int i = 4; i <= 11; i++)
-                        {
-                            var enemyTile = (AtkComponentNode*)enlistAtk->UldManager.NodeList[i];
-                            if (enemyTile->AtkResNode.IsVisible)
-                            {
-                                if (enemyTile->Component->UldManager.NodeListCount < 11) continue;
-                                var enemyBar = (AtkImageNode*)enemyTile->Component->UldManager.NodeList[10];
-                                drawList.Add((enemyTile->AtkResNode.X * enlistAtk->Scale + baseX - 2f,
-                                    enemyTile->AtkResNode.Y * enlistAtk->Scale + enemyTile->AtkResNode.Height * enlistAtk->Scale / 2f + baseY,
-                                    (enemyBar->AtkResNode.ScaleX * 100f).ToString("0")+"%"));
-                            }
-
-                        }
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                PluginLog.Error("[EnmityHp] " + e.Message + "\n" + e.StackTrace);
-            }
-        }
-
         private void Draw()
         {
             if (CfgOpen)
             {
-                if(ImGui.Begin("EnmityHp configuration", ref CfgOpen, ImGuiWindowFlags.AlwaysAutoResize))
+                if(ImGui.Begin("EnemyListHP configuration", ref CfgOpen, ImGuiWindowFlags.AlwaysAutoResize))
                 {
                     ImGui.Checkbox("Left align (right otherwise)", ref Cfg.IsLeftAligned);
                     ImGui.SetNextItemWidth(100f);
@@ -96,24 +53,59 @@ namespace EnmityHp
                 if (!CfgOpen)
                 {
                     Svc.PluginInterface.SavePluginConfig(Cfg);
-                    Svc.PluginInterface.UiBuilder.AddNotification("EnmityHp", "Configuration saved", NotificationType.Success);
+                    Svc.PluginInterface.UiBuilder.AddNotification("EnemyListHP", "Configuration saved", NotificationType.Success);
                 }
             }
-            for (int i = 0;i<drawList.Count;i++)
+            if (Svc.Condition[ConditionFlag.InCombat] && !Svc.ClientState.IsPvP)
             {
-                ImGuiHelpers.ForceNextWindowMainViewport();
-                var textSize = ImGui.CalcTextSize(drawList[i].str) * Cfg.Size;
-                ImGuiHelpers.SetNextWindowPosRelativeMainViewport(new Vector2(drawList[i].x - (Cfg.IsLeftAligned?0:textSize.X) + Cfg.OffsetX, drawList[i].y - textSize.Y / 2f + Cfg.OffsetY));
-                ImGui.PushStyleVar(ImGuiStyleVar.WindowPadding, new Vector2(2f, 0f));
-                ImGui.PushStyleVar(ImGuiStyleVar.WindowMinSize, new Vector2(0f, 0f));
-                ImGui.Begin("##enmityhp"+i, ImGuiWindowFlags.NoInputs | ImGuiWindowFlags.NoScrollbar
-                    | ImGuiWindowFlags.AlwaysAutoResize | ImGuiWindowFlags.NoMouseInputs | ImGuiWindowFlags.NoNavFocus 
-                    | ImGuiWindowFlags.AlwaysUseWindowPadding | ImGuiWindowFlags.NoTitleBar | ImGuiWindowFlags.NoSavedSettings);
-                ImGui.SetWindowFontScale(Cfg.Size);
-                ImGui.TextUnformatted(drawList[i].str);
-                ImGui.End();
-                ImGui.PopStyleVar(2);
+                try
+                {
+                    var enlist = Svc.GameGui.GetAddonByName("_EnemyList", 1);
+                    if (enlist != IntPtr.Zero)
+                    {
+                        var enlistAtk = (AtkUnitBase*)enlist;
+                        if (enlistAtk->UldManager.NodeListCount < 12) return;
+                        var baseX = enlistAtk->X;
+                        var baseY = enlistAtk->Y;
+                        if (enlistAtk->IsVisible)
+                        {
+                            for (int i = 4; i <= 11; i++)
+                            {
+                                var enemyTile = (AtkComponentNode*)enlistAtk->UldManager.NodeList[i];
+                                if (enemyTile->AtkResNode.IsVisible)
+                                {
+                                    if (enemyTile->Component->UldManager.NodeListCount < 11) continue;
+                                    var enemyBar = (AtkImageNode*)enemyTile->Component->UldManager.NodeList[10];
+                                    DrawEnemyHp(enemyTile->AtkResNode.X * enlistAtk->Scale + baseX - 2f,
+                                        enemyTile->AtkResNode.Y * enlistAtk->Scale + enemyTile->AtkResNode.Height * enlistAtk->Scale / 2f + baseY,
+                                        (enemyBar->AtkResNode.ScaleX * 100f).ToString("0") + "%");
+                                }
+
+                            }
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    PluginLog.Error(e.Message + "\n" + e.StackTrace);
+                }
             }
+        }
+
+        void DrawEnemyHp(float x, float y, string str)
+        {
+            ImGuiHelpers.ForceNextWindowMainViewport();
+            var textSize = ImGui.CalcTextSize(str) * Cfg.Size;
+            ImGuiHelpers.SetNextWindowPosRelativeMainViewport(new Vector2(x - (Cfg.IsLeftAligned ? 0 : textSize.X) + Cfg.OffsetX, y - textSize.Y / 2f + Cfg.OffsetY));
+            ImGui.PushStyleVar(ImGuiStyleVar.WindowPadding, new Vector2(2f, 0f));
+            ImGui.PushStyleVar(ImGuiStyleVar.WindowMinSize, new Vector2(0f, 0f));
+            ImGui.Begin("##enmityhp" + y, ImGuiWindowFlags.NoInputs | ImGuiWindowFlags.NoScrollbar
+                | ImGuiWindowFlags.AlwaysAutoResize | ImGuiWindowFlags.NoMouseInputs | ImGuiWindowFlags.NoNavFocus
+                | ImGuiWindowFlags.AlwaysUseWindowPadding | ImGuiWindowFlags.NoTitleBar | ImGuiWindowFlags.NoSavedSettings);
+            ImGui.SetWindowFontScale(Cfg.Size);
+            ImGui.TextUnformatted(str);
+            ImGui.End();
+            ImGui.PopStyleVar(2);
         }
     }
 }
--- EnmityHp/Svc.cs
+++ EnmityHp/Svc.cs
@@ -24,17 +24,19 @@ namespace EnmityHp
 	class Svc
 	{
 		[PluginService] static internal DalamudPluginInterface PluginInterface { get; private set; }
-		[PluginService] static internal BuddyList Buddies { get; private set; }
-		[PluginService] static internal ChatGui Chat { get; private set; }
-		[PluginService] static internal ChatHandlers ChatHandlers { get; private set; }
+		//[PluginService] static internal BuddyList Buddies { get; private set; }
+		//[PluginService] static internal ChatGui Chat { get; private set; }
+		//[PluginService] static internal ChatHandlers ChatHandlers { get; private set; }
 		[PluginService] static internal ClientState ClientState { get; private set; }
-		[PluginService] static internal CommandManager Commands { get; private set; }
+		//[PluginService] static internal CommandManager Commands { get; private set; }
 		[PluginService] static internal Condition Condition { get; private set; }
-		[PluginService] static internal DataManager Data { get; private set; }
-		[PluginService] static internal FateTable Fates { get; private set; }
-		[PluginService] static internal FlyTextGui FlyText { get; private set; }
+		//[PluginService] static internal DataManager Data { get; private set; }
+		//[PluginService] static internal FateTable Fates { get; private set; }
+		/*[PluginService] static internal FlyTextGui FlyText { get; private set; }
 		[PluginService] static internal Framework Framework { get; private set; }
+		*/
 		[PluginService] static internal GameGui GameGui { get; private set; }
+		/*
 		[PluginService] static internal GameNetwork GameNetwork { get; private set; }
 		[PluginService] static internal JobGauges Gauges { get; private set; }
 		[PluginService] static internal KeyState KeyState { get; private set; }
@@ -45,6 +47,6 @@ namespace EnmityHp
 		[PluginService] static internal SeStringManager SeStringManager { get; private set; }
 		[PluginService] static internal SigScanner SigScanner { get; private set; }
 		[PluginService] static internal TargetManager Targets { get; private set; }
-		[PluginService] static internal ToastGui Toasts { get; private set; }
+		[PluginService] static internal ToastGui Toasts { get; private set; }*/
 	}
 }
